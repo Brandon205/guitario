508e6385654d461e94e2af0c5bd31cc77f5e1d1d
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-10-02 16:56:44 -0700

    Initial commit

---
+++ components/Notes.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react'
+import { View, Text } from 'react-native'
+
+export default function Notes() {
+    return (
+        <View>
+            <Text>Learn notes here!</Text>
+        </View>
+    )
+}
+
