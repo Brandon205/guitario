f70ada6c4f82009209e7eeab694f346cbbae6aaf
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-10-04 11:37:38 -0700

    Changed some styling, added note/string colors

--- components/Notes.js
+++ components/Notes.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect} from 'react'
 import { View, Text, Platform, StyleSheet, Dimensions} from 'react-native'
-// import frets from './components/frets.json';
 const frets = require('./frets.json')
 
 export default function Notes() {
     const [note, setNote] = useState(undefined)
     const [string, setString] = useState(undefined)
+    const [noteColor, setNoteColor] = useState(undefined)
+    const [stringColor, setStringColor] = useState(undefined)
     const [answer, setAnswer] = useState(undefined)
 
     useEffect(() => {
         if (Platform.OS === 'web') {
-            // Register keyboard event here if needed
+            document.addEventListener('keypress', (e) => {
+                if (e.code === 'Space') {
+                    createNote()
+                }
+            })
         }
 
         createNote()
 
         return () => {
-            // Unregister keyboard event here if needed
-            // if (Platform.OS === 'web') {
-            //     // Unregister keyboard event
-            // }
+            if (Platform.OS === 'web') {
+                document.removeEventListener('keypress')
+            }
         }
     }, [])
 
@@ -30,24 +34,31 @@ export default function Notes() {
         switch (tempNote) {
             case "0":
                 setNote("A")
+                setNoteColor('#33CC33')
                 break;
             case "1":
                 setNote("B")
+                setNoteColor('#8EC9FF')
                 break;
             case "2":
                 setNote("C")
+                setNoteColor('#FF0000')
                 break;
             case "3":
                 setNote("D")
+                setNoteColor('#FFFF00')
                 break;
             case "4":
                 setNote("E")
+                setNoteColor('#C3F2FF')
                 break;
             case "5":
                 setNote("F")
+                setNoteColor('#AB0034')
                 break;
             case "6":
                 setNote("G")
+                setNoteColor('#FF7F00')
                 break;
             default:
                 setNote("Uh oh, this should be a note...")
@@ -56,21 +67,27 @@ export default function Notes() {
         switch (tempString) {
             case "0":
                 setString("E (6th)")
+                setStringColor('#D07173')
                 break;
             case "1":
                 setString("A (5th)")
+                setStringColor('#F3F071')
                 break;
             case "2":
                 setString("D (4th)")
+                setStringColor('#1861BF')
                 break;
             case "3":
                 setString("G (3rd)")
+                setStringColor('#F3B54D')
                 break;
             case "4":
                 setString("B (2nd)")
+                setStringColor('#50C839')
                 break;
             case "5":
                 setString("e (1st)")
+                setStringColor('#CF27C4')
                 break;
             default:
                 setString("Uh oh, this should be a string...")
@@ -81,9 +98,9 @@ export default function Notes() {
 
     return (
         <View style={styles.container} onTouchStart={() => createNote()}>
-            <Text style={styles.noteText}>String: {string}</Text>
-            <Text style={styles.noteText}>Note: {note}</Text>
-            <Text style={{color: 'black'}}>*Tap anywhere on the screen to get a new set of notes</Text>
+            <Text style={styles.noteText}>String: <Text style={{color: stringColor}}>{string}</Text></Text>
+            <Text style={styles.noteText}>Note: <Text style={{color: noteColor}}>{note}</Text></Text>
+            <Text style={{color: 'white'}}>*Tap anywhere on the screen to get a new set of notes</Text>
             <Text style={styles.answer}>Correct Fret: {answer} *Hide this in the settings</Text>
         </View>
     )
@@ -95,15 +112,16 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'whitesmoke',
+        backgroundColor: '#292B36',
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height
     },
     noteText: {
         fontSize: 50,
-        color: 'black'
+        color: 'white'
     },
     answer: {
-        fontSize: 8
+        fontSize: 8,
+        color: 'white'
     }
 })
