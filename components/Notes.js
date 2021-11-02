ab3be8bcfe96faa45e1371d2d00c31c7dae59e3f
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-11-02 10:29:02 -0700

    Added StringsNote functionality

--- components/Notes.js
+++ components/Notes.js
@@ -14,7 +14,7 @@ export default function Notes(props) {
         <View style={styles.container} onTouchStart={() => playThis()}>
             <Text style={styles.noteText}>String: <Text style={{color: props.stringColor}}>{props.string}</Text></Text>
             <Text style={styles.noteText}>Note: <Text style={{color: props.noteColor}}>{props.note}</Text></Text>
-            <Text style={{color: '#000'}}>*Tap anywhere on the screen to get a new note to play</Text>
+            <Text style={{color: '#fff'}}>*Tap anywhere on the screen to get a new note to play</Text>
             <TouchableOpacity style={styles.answer} onLongPress={() => setShowAnswer(true)} onPressOut={() => setShowAnswer(false)}><Text style={styles.answer}>*Press and hold here to see the correct fret</Text></TouchableOpacity>
             <Text style={{display: showAnswer ? 'flex' : 'none', color: 'white', fontSize: 20}}>{frets[props.string][props.note]}</Text>
         </View>
--- components/StringsNote.js
+++ components/StringsNote.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 
 export default function StringsNote(props) {
     return (
-        <View>
-            <Text>Play this note on all of the strings: A</Text>
+        <View style={styles.container} onTouchStart={() => props.createNote()}>
+            <Text style={styles.screenText}>Note: <Text style={{color: props.noteColor}}>{props.note}</Text></Text>
+            <Text style={{color: '#fff', fontWeight: 'bold'}}>*Play the above note on all of the strings</Text>
+            <Text style={{color: '#fff'}}>*Tap anywhere on the screen to get a new note to play</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#292B36',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+    }, 
+    screenText: {
+        fontSize: 50,
+        color: 'white'
+    },
+    
+})
