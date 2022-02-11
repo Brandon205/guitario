0e5dbbb067600400ba803f91f98404019997f4b7
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-02-10 18:09:29 -0800

    Started moving things over, no testing done yet

--- components/Frequencies.js
+++ components/Frequencies.js
@@ -1,40 +1,98 @@
 import React, { useState, useEffect } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
-import { Audio } from 'expo-av';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { noteFromPitch, centsOffFromPitch } from "../helpers/helper.js";
+import autoCorrelate from "../helpers/autoCorrelate.js";
+
+const audioCtx = new AudioContext();
+const analyserNode = audioCtx.createAnalyser();
+const buflen = 2048;
+var buf = new Float32Array(buflen);
+
+const noteStrings = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
 
 export default function Frequencies() {
-    const [recording, setRecording] = React.useState();
+    const [source, setSource] = useState(null);
+    const [started, setStart] = useState(false);
+    const [pitchNote, setPitchNote] = useState("C");
+    const [pitchScale, setPitchScale] = useState("4");
+    const [pitch, setPitch] = useState("0");
 
-    async function startRecording() {
-    try {
-        console.log('Requesting permissions..');
-        await Audio.requestPermissionsAsync();
-        await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-        }); 
-        console.log('Starting recording..');
-        const { recording } = await Audio.Recording.createAsync(
-            Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-        );
-        setRecording(recording);
-        console.log('Recording started');
-    } catch (err) {
-        console.error('Failed to start recording', err);
+    const updatePitch = (time) => {
+        analyserNode.getFloatTimeDomainData(buf);
+        var ac = autoCorrelate(buf, audioCtx.sampleRate);
+        if (ac > -1) {
+          let note = noteFromPitch(ac);
+          let sym = noteStrings[note % 12];
+          let scl = Math.floor(note / 12) - 1;
+          let dtune = centsOffFromPitch(ac, note);
+          setPitch(parseFloat(ac).toFixed(2) + " Hz");
+          setPitchNote(sym);
+          setPitchScale(scl);
+          setDetune(dtune);
+          setNotification(false);
+          console.log(note, sym, scl, dtune, ac);
         }
-    }
+      };
 
-    async function stopRecording() { // TEST RECORDING AT file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540brandonb205%252Fguitario/Audio/recording-8b74acc7-9ab6-441e-a4d8-18371302edc5.m4a
-    console.log('Stopping recording..');
-    // setRecording(undefined); // Maybe get rid of this to have an access point to the latest recording?
-    await recording.stopAndUnloadAsync();
-    const uri = recording.getURI(); // Might use this to access the stored audio file
-    console.log('Recording stopped and stored at', uri);
+      useEffect(() => {
+        if (source != null) {
+          source.connect(analyserNode);
+        }
+      }, [source]);
+    
+      setInterval(updatePitch, 1);
+    
+      const start = async () => {
+        const input = await getMicInput();
+    
+        if (audioCtx.state === "suspended") {
+          await audioCtx.resume();
+        }
+        setStart(true);
+        setNotification(true);
+        setTimeout(() => setNotification(false), 5000);
+        setSource(audioCtx.createMediaStreamSource(input));
+      };
+    
+      const stop = () => {
+        source.disconnect(analyserNode);
+        setStart(false);
+      };
+    
+      const getMicInput = () => {
+        return navigator.mediaDevices.getUserMedia({
+          audio: {
+            echoCancellation: true,
+            autoGainControl: false,
+            noiseSuppression: false,
+            latency: 0,
+          },
+        });
+      };
+
+    if (pitchNote === "A") { // TESTING OF LATER IMPLEMENTATION
+        console.log("WINNER")
     }
 
     return (
         <View style={styles.container}>
-            <Button title={recording ? 'Stop Recording' : 'Start Recording'} onPress={recording ? stopRecording : startRecording} />
+            <Button title="Start" onPress={start} />
+            <Text>{pitchNote}</Text>
+            <Text>{pitch}</Text>
+            <Button title="Stop" onPress={stop} />
         </View>
     )
 }
